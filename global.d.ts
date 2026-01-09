818638c025330ad8dfe0991702c9ba244004e402
Dmitry Podkopaev <dmitry.podkopaev@ladadigit.io>
2026-01-09 23:22:10 +0300

    homework refactoring for typescript

--- global.d.ts
+++ global.d.ts
@@ -21,7 +21,7 @@ declare global {
       NODE_ENV: 'development' | 'production' | 'test';
       PORT?: number;
       DATABASE_URL: string;
-      SECRET_KEY: string;
+      REDIS_URL?: string;
     }
   }
 }
